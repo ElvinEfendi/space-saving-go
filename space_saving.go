6b358b90727e6bac5a6dc61d903db777541c5c98
Elvin Efendi <elvin.efendiyev@gmail.com>
2017-01-31 00:43:50 -0500

    debug statements

--- space_saving.go
+++ space_saving.go
@@ -1,22 +1,42 @@
 package space_saving
 
+import (
+	log "github.com/Sirupsen/logrus"
+)
+
 type SpaceSaving struct {
 	streamSummary  *StreamSummary
 	maxCountersLen int
 }
 
+func init() {
+	log.SetLevel(log.DebugLevel)
+}
+
 func New(maxCountersLen int) *SpaceSaving {
+	log.WithField("max_counters_len", maxCountersLen).Debug("Initialized a new SpaceSaving instance")
 	return &SpaceSaving{maxCountersLen: maxCountersLen, streamSummary: NewStreamSummary()}
 }
 
 func (ss *SpaceSaving) Process(key string) error {
+	var err error
 	if ss.streamSummary.HasKey(key) {
-		return ss.streamSummary.Increment(key)
+		err = ss.streamSummary.Increment(key)
+	} else if ss.streamSummary.Len() < ss.maxCountersLen {
+		err = ss.streamSummary.Add(key)
+	} else {
+		err = ss.streamSummary.ReplaceWith(key)
 	}
-	if ss.streamSummary.Len() < ss.maxCountersLen {
-		return ss.streamSummary.Add(key)
-	}
-	return ss.streamSummary.ReplaceWith(key)
+
+	log.WithFields(log.Fields{"number_of_buckets": ss.streamSummary.bucketList.Len(),
+		"number_of_counters": ss.getNumberOfCounters(),
+		"front_key":          ss.streamSummary.bucketList.Front().Value.(*Bucket).counterList.Back().Value.(*Counter).key,
+		"front_value":        ss.streamSummary.bucketList.Front().Value.(*Bucket).value,
+		"back_key":           ss.streamSummary.bucketList.Back().Value.(*Bucket).counterList.Front().Value.(*Counter).key,
+		"back_value":         ss.streamSummary.bucketList.Back().Value.(*Bucket).value}).
+		Debug("Stats")
+
+	return err
 }
 
 func (ss *SpaceSaving) Top(k int) ([]string, []int) {
@@ -32,4 +52,13 @@ func (ss *SpaceSaving) Top(k int) ([]string, []int) {
 		}
 	}
 	return keys, counts
+}
+
+func (ss *SpaceSaving) getNumberOfCounters() int {
+	count := 0
+	for bucketElement := ss.streamSummary.bucketList.Front(); bucketElement != nil; bucketElement = bucketElement.Next() {
+		bucket := bucketElement.Value.(*Bucket)
+		count += bucket.counterList.Len()
+	}
+	return count
 }
--- stream_summary.go
+++ stream_summary.go
@@ -3,6 +3,7 @@ package space_saving
 import (
 	"container/list"
 	"fmt"
+	log "github.com/Sirupsen/logrus"
 )
 
 type Bucket struct {
@@ -34,12 +35,12 @@ func (ss *StreamSummary) Len() int {
 }
 
 func (ss *StreamSummary) Add(key string) error {
-	fmt.Printf("Adding %s\n", key)
+	log.WithField("key", key).Info("Add")
 	return ss.addWithCount(key, 1)
 }
 
 func (ss *StreamSummary) Increment(key string) error {
-	fmt.Printf("Incrementing %s\n", key)
+	log.WithField("key", key).Info("Increment")
 	counterElement, ok := ss.counters[key]
 	if !ok {
 		return fmt.Errorf("%s does not exist", key)
@@ -48,28 +49,22 @@ func (ss *StreamSummary) Increment(key string) error {
 	counter := counterElement.Value.(*Counter)
 	bucketElement := counter.bucketElement
 	bucket := bucketElement.Value.(*Bucket)
-	fmt.Printf("  count for %s is %d counterList.size = %d\n", key, bucket.value, bucket.counterList.Len())
 	newCount := bucket.value + 1
 	if bucketElement = bucketElement.Prev(); bucketElement == nil || bucketElement.Value.(*Bucket).value > newCount {
 		if bucket.counterList.Len() == 1 {
 			bucket.value = newCount
 			counter.bucketElement.Value = bucket
 			ss.counters[key] = counterElement
-			fmt.Printf("  [bucket.counterList.Len() == 1] new count for %s is %d, counterList.size = %d\n", key, bucket.value, bucket.counterList.Len())
 			return nil
 		}
 		bucket = &Bucket{value: newCount}
 		bucket.counterList = list.New()
-		fmt.Printf("  Creating a bucket %s, %d\n", key, newCount)
 		bucketElement = ss.bucketList.InsertBefore(bucket, counter.bucketElement)
 	}
 	ss.removeCounterFromBucket(counterElement)
-	fmt.Printf("  PrevBucketValue = %d, newCount = %d\n", bucketElement.Value.(*Bucket).value, newCount)
 	bucketElement.Value.(*Bucket).value = newCount
 	counter.bucketElement = bucketElement
 	ss.counters[key] = bucketElement.Value.(*Bucket).counterList.PushFront(counter)
-	fmt.Printf("  new count for %s is %d, counterList.size = %d, ss.counters.counterList.size = %d\n",
-		key, bucketElement.Value.(*Bucket).value, bucket.counterList.Len(), ss.counters[key].Value.(*Counter).bucketElement.Value.(*Bucket).counterList.Len())
 	return nil
 }
 
@@ -77,7 +72,7 @@ func (ss *StreamSummary) ReplaceWith(key string) error {
 	bucketElement := ss.bucketList.Back()
 	counterElement := bucketElement.Value.(*Bucket).counterList.Front()
 	keyToBeReplaced := counterElement.Value.(*Counter).key
-	fmt.Printf("Replacing %s with %s\n", keyToBeReplaced, key)
+	log.WithFields(log.Fields{"from_key": keyToBeReplaced, "to_key": key}).Info("ReplaceWith")
 	delete(ss.counters, keyToBeReplaced)
 	ss.removeCounterFromBucket(counterElement)
 	ss.addWithCount(key, bucketElement.Value.(*Bucket).value)
@@ -86,9 +81,7 @@ func (ss *StreamSummary) ReplaceWith(key string) error {
 
 func (ss *StreamSummary) addWithCount(key string, count int) error {
 	counter := &Counter{key: key, bucketElement: ss.bucketList.Back()}
-	fmt.Printf("  addWithCount %s, %d\n", key, count)
 	if counter.bucketElement == nil || counter.bucketElement.Value.(*Bucket).value > count {
-		fmt.Printf("  Creating a bucket %s, %d\n", key, count)
 		bucket := &Bucket{value: count}
 		bucket.counterList = list.New()
 		counter.bucketElement = ss.bucketList.PushBack(bucket)
@@ -99,11 +92,9 @@ func (ss *StreamSummary) addWithCount(key string, count int) error {
 
 func (ss *StreamSummary) removeCounterFromBucket(counterElement *list.Element) {
 	bucketElement := counterElement.Value.(*Counter).bucketElement
-	fmt.Printf("  removing counter element with bucket value %d\n", bucketElement.Value.(*Bucket).value)
 	counterList := bucketElement.Value.(*Bucket).counterList
 	counterList.Remove(counterElement)
 	if counterList.Len() == 0 {
-		fmt.Printf("  removing bucketElement\n")
 		ss.bucketList.Remove(bucketElement)
 	}
 }
